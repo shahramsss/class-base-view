c8642b9ae267573590e0d5f52f9f69ab109b80da
sss <shahramsss1987@gmail.com>
2024-08-21 12:12:52 +0330

    add options method and limit http methods

--- home/views.py
+++ home/views.py
@@ -1,6 +1,31 @@
+from typing import Any
+from django.http import HttpRequest
+from django.http.response import HttpResponse as HttpResponse
 from django.shortcuts import render
 from django.views import View
 
+
 class Home(View):
-    def get(self , request):
-        return render(request, 'home/home.html')
+    # http_method_names = ["post"]
+
+    def get(self, request):
+        return render(request, "home/home.html")
+
+    def options(self, request: HttpRequest, *args: Any, **kwargs: Any) -> HttpResponse:
+        response = super().options(request, *args, **kwargs)
+        response.headers["host"] = "localhost"
+        response.headers["user"] = request.user
+        return response
+
+    def post(self, request):
+        return render(request, "home/home.html")
+
+    def options(self, request: HttpRequest, *args: Any, **kwargs: Any) -> HttpResponse:
+        response = super().options(request, *args, **kwargs)
+        response.headers["host"] = "localhost"
+        response.headers["user"] = request.user
+        return response
+    
+    def http_method_not_allowed(self, request: HttpRequest, *args: Any, **kwargs: Any) -> HttpResponse:
+        return render(request , 'method_not_allowd.html')
+    
